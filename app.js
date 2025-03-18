20823d09b06421ff74fca1db0a11631afcc84f86
SyedAsharBukhari <asharbukhari649@gmai.com>
2025-03-18 15:04:21 -0700

    working

---
+++ app.js
@@ -0,0 +1,65 @@
+// let user = prompt("enter city")
+
+// let user1 = document.write(` '<img src="images/sunny.webp" alt=""  style="width: 200px; height: 200px; ">'`)
+
+// if (user === "karachi") {
+//     document.write(user1)
+// }
+
+
+
+
+
+// document.write(` <div class="container d-flex justify-content-center align-items-center mt-5">
+   
+//     <div class="rounded mt-3"
+//       style="background-color: rgb(174, 197, 171); border: 1px solid black; width: 50%; height: 40%;">
+//       <div class=" d-flex justify-content-center align-items-center flex-column">
+//         <div class=" rounded-circle "
+//           style="width: 200px; height: 200px; background-color: blue;"></div>
+//           <div class="mt-3" style="height: 80px; width: 300px; background-color: brown;"></div>
+//           <div class="mt-3 " style="height: 80px; width: 300px; background-color: rgb(69, 42, 165);"></div>
+//       </div>
+
+//     </div>
+//   </div>`)
+
+
+
+document.write ( "<nav class='navbar navbar-expand-lg navbar-light bg-primary fixed-bottom'>"+
+    "<div class='container-fluid'>" +
+      "<a class='navbar-brand fw-bold fs-1' href='#'>" + "CheckWiz" + "</a>"+
+      "<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" +
+        "<span class='navbar-toggler-icon'>"+"</span>" +
+      "</button>"+
+      "<div class='collapse navbar-collapse' id='navbarSupportedContent'>"+
+        "<ul class='navbar-nav  mx-5 fs-3 mb-lg-0'>"+
+          "<li class='nav-item mx-3 fw-bold'>"+
+            "<a class='nav-link active' aria-current='page' href='#'>" + "Karchi" + "</a>" +
+          "</li>" +
+
+          "<li class='nav-item mx-3 fw-bold'>"+
+           "<a class='nav-link active' aria-current='page' href='#'>" + "Lahore" + "</a>" +
+          "</li>"+
+
+          "<li class='nav-item mx-3 fw-bold'>"+
+            "<a class='nav-link active' aria-current='page' href='#'>"+ "Islamabad" + "</a>" +
+          "</li>" +
+
+          "<li class='nav-item mx-3 fw-bold'>" +
+            "<a class='nav-link active' aria-current='page' href='#'>" + "Muree" + "</a>" +
+          "</li>" +
+
+          "<li class='nav-item mx-3 fw-bold'>"+
+            "<a class='nav-link active' aria-current='page' href='#'>" + "Quetta" + "</a>" +
+          "</li>" +
+
+          "<li class='nav-item mx-3 fw-bold'>" +
+            "<a class='nav-link active' aria-current='page' href='#'>" + "Peshawar" + "</a>"+
+          "</li>"+
+    
+        "</ul>"+
+        
+      "</div>" +
+    "</div>" +
+  "</nav>")
