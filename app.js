37ce99f4c4b76a874c317c6997235443f5105409
SyedAsharBukhari <asharbukhari649@gmai.com>
2025-03-19 15:34:39 -0700

    done

--- app.js
+++ app.js
@@ -1,65 +1,220 @@
-// let user = prompt("enter city")
-
-// let user1 = document.write(` '<img src="images/sunny.webp" alt=""  style="width: 200px; height: 200px; ">'`)
-
-// if (user === "karachi") {
-//     document.write(user1)
-// }
+let usercity = prompt("Enter Your City Name !")
 
 
 
+if (usercity.toLocaleLowerCase() === "karachi") {
 
-
-// document.write(` <div class="container d-flex justify-content-center align-items-center mt-5">
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
    
-//     <div class="rounded mt-3"
-//       style="background-color: rgb(174, 197, 171); border: 1px solid black; width: 50%; height: 40%;">
-//       <div class=" d-flex justify-content-center align-items-center flex-column">
-//         <div class=" rounded-circle "
-//           style="width: 200px; height: 200px; background-color: blue;"></div>
-//           <div class="mt-3" style="height: 80px; width: 300px; background-color: brown;"></div>
-//           <div class="mt-3 " style="height: 80px; width: 300px; background-color: rgb(69, 42, 165);"></div>
-//       </div>
+    <div class="rounded mt-3 " style="background-color: rgb(41, 40, 38);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(88, 32, 192);">
+      
+      <div class=" d-flex justify-content-center align-items-center flex-column">
+       
+        <div class=" rounded-circle cir-img mt-2"
+          style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+          <img src="images/kar.webp" alt="">
+        </div>
+          
+        <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151) ;   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+          <h2>32 °C</h2>
+        </div>
+          <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+            <h1>Karachi</h1>
+          </div>
+      </div>
 
-//     </div>
-//   </div>`)
+    </div>
+  </div> `)
 
 
 
-document.write ( "<nav class='navbar navbar-expand-lg navbar-light bg-primary fixed-bottom'>"+
-    "<div class='container-fluid'>" +
-      "<a class='navbar-brand fw-bold fs-1' href='#'>" + "CheckWiz" + "</a>"+
-      "<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" +
-        "<span class='navbar-toggler-icon'>"+"</span>" +
-      "</button>"+
-      "<div class='collapse navbar-collapse' id='navbarSupportedContent'>"+
-        "<ul class='navbar-nav  mx-5 fs-3 mb-lg-0'>"+
-          "<li class='nav-item mx-3 fw-bold'>"+
-            "<a class='nav-link active' aria-current='page' href='#'>" + "Karchi" + "</a>" +
-          "</li>" +
+} else if (usercity.toLocaleLowerCase() === "lahore") {
 
-          "<li class='nav-item mx-3 fw-bold'>"+
-           "<a class='nav-link active' aria-current='page' href='#'>" + "Lahore" + "</a>" +
-          "</li>"+
 
-          "<li class='nav-item mx-3 fw-bold'>"+
-            "<a class='nav-link active' aria-current='page' href='#'>"+ "Islamabad" + "</a>" +
-          "</li>" +
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
+   
+    <div class="rounded mt-3 " style="background-color: rgb(226, 198, 147);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(192, 32, 32);">
+      
+      <div class=" d-flex justify-content-center align-items-center flex-column">
+       
+        <div class=" rounded-circle cir-img mt-2"
+          style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+          <img src="/images/cold 1.webp" alt="">
+        </div>
+          
+        <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(197, 157, 157);   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+          <h2>17 °C</h2>
+        </div>
+          <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+            <h1>Lahore</h1>
+          </div>
+      </div>
 
-          "<li class='nav-item mx-3 fw-bold'>" +
-            "<a class='nav-link active' aria-current='page' href='#'>" + "Muree" + "</a>" +
-          "</li>" +
+    </div>
+  </div> `)
 
-          "<li class='nav-item mx-3 fw-bold'>"+
-            "<a class='nav-link active' aria-current='page' href='#'>" + "Quetta" + "</a>" +
-          "</li>" +
+} else if (usercity.toLocaleLowerCase() === "islamabad") {
 
-          "<li class='nav-item mx-3 fw-bold'>" +
-            "<a class='nav-link active' aria-current='page' href='#'>" + "Peshawar" + "</a>"+
-          "</li>"+
-    
-        "</ul>"+
+
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
+   
+    <div class="rounded mt-3 " style="background-color: rgb(226, 198, 147);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(192, 32, 32);">
+      
+      <div class=" d-flex justify-content-center align-items-center flex-column">
+       
+        <div class=" rounded-circle cir-img mt-2"
+          style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+          <img src="images/normal.webp" alt="">
+        </div>
+          
+        <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(197, 157, 157);   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+          <h2>27 °C</h2>
+        </div>
+          <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+            <h1>Islamabad</h1>
+          </div>
+      </div>
+
+    </div>
+  </div> `)
+
+} else if (usercity.toLocaleLowerCase() === "murre") {
+
+
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
+     
+      <div class="rounded mt-3 " style="background-color: rgb(226, 198, 147);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(192, 32, 32);">
         
-      "</div>" +
-    "</div>" +
+        <div class=" d-flex justify-content-center align-items-center flex-column">
+         
+          <div class=" rounded-circle cir-img mt-2"
+            style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+            <img src="images/murre.webp" alt="">
+          </div>
+            
+          <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(197, 157, 157);   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+            <h2>8 °C</h2>
+          </div>
+            <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+              <h1>Murre</h1>
+            </div>
+        </div>
+  
+      </div>
+    </div> `)
+
+}
+else if (usercity.toLocaleLowerCase() === "quetta") {
+
+
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
+       
+        <div class="rounded mt-3 " style="background-color: rgb(226, 198, 147);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(192, 32, 32);">
+          
+          <div class=" d-flex justify-content-center align-items-center flex-column">
+           
+            <div class=" rounded-circle cir-img mt-2"
+              style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+              <img src="images/quee.webp" alt="">
+            </div>
+              
+            <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(197, 157, 157);   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+              <h2>10 °C</h2>
+            </div>
+              <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+                <h1>Quetta</h1>
+              </div>
+          </div>
+    
+        </div>
+      </div> `)
+
+}
+else if (usercity.toLocaleLowerCase() === "peshawar") {
+
+
+  document.write(` <div class="container d-flex justify-content-center align-items-center mt-3">
+         
+          <div class="rounded mt-3 " style="background-color: rgb(226, 198, 147);  width: 40%; height: 60%; box-shadow: 5px 5px 6px rgb(192, 32, 32);">
+            
+            <div class=" d-flex justify-content-center align-items-center flex-column">
+             
+              <div class=" rounded-circle cir-img mt-2"
+                style="width: 200px; height: 200px; background-color: blue; box-shadow: 5px 5px 6px rgbrgb(32, 43, 192);">
+                <img src="images/pes.webp" alt="">
+              </div>
+                
+              <div class="mt-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(197, 157, 157);   box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+                <h2>28 °C</h2>
+              </div>
+                <div class="mt-3 mb-3 rounded  d-flex justify-content-center align-items-center" style="height: 80px; width: 300px; background-color: rgb(179, 151, 151); box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+                  <h1>Peshawar</h1>
+                </div>
+            </div>
+      
+          </div>
+        </div> `)
+
+} else {
+  document.write(`   <div class=" cir-img2   mt-5 m-auto"
+                style="width: 400px; height: 400px; background-color: blue; box-shadow: 5px 5px 6px rgb(32, 43, 192);">
+                <img src="images/else.webp" alt="">
+              </div>`)
+}
+
+
+
+
+document.write("<nav class='navbar navbar-expand-lg navbar-light bg-primary fixed-bottom'>" +
+  "<div class='container-fluid'>" +
+  "<a class='navbar-brand fw-bold fs-1' href='#'>" + "CheckWiz" + "</a>" +
+  "<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" +
+  "<span class='navbar-toggler-icon'>" + "</span>" +
+  "</button>" +
+  "<div class='collapse navbar-collapse' id='navbarSupportedContent'>" +
+  "<ul class='navbar-nav  mx-5 fs-3 mb-lg-0'>" +
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Karchi" + "</a>" +
+  "</li>" +
+
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Lahore" + "</a>" +
+  "</li>" +
+
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Islamabad" + "</a>" +
+  "</li>" +
+
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Muree" + "</a>" +
+  "</li>" +
+
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Quetta" + "</a>" +
+  "</li>" +
+
+  "<li class='nav-item mx-3 fw-bold'>" +
+  "<a class='nav-link active' aria-current='page' href='#'>" + "Peshawar" + "</a>" +
+  "</li>" +
+
+  "</ul>" +
+
+  "</div>" +
+  "</div>" +
   "</nav>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
